9f35dc797ee2f778288c82e1af682a6fd516999f
Leidy333 <tamy_2011@yahoo.com>
2023-11-09 17:16:25 -0800

    adding prior test list

---
+++ workshop18p1.js
@@ -0,0 +1,13 @@
+//Unit test
+//Multiplication function
+    //Expect 5 x 3 to be 15
+    //Expect -5 x 3 to be -15
+    //Expect y x 5 to be an error
+    //Expect more than two numbers to be an error message
+    //Expect to negative integers to be a posotive result
+
+//A Function called "contactOdds"
+    //Expect two inputs to be arrays
+    //Expect input of string arrays to be an error
+    //Expect input of arrays with non numericals to be an error 
+    //Expect  
